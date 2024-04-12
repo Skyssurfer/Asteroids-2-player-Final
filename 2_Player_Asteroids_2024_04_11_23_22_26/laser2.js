4021d4e4b665d620a9954ea608cb5146c395c91c
DaOGN00B <156867723+DaOGN00B@users.noreply.github.com>
2024-04-11 17:48:56 -0700

    Update laser2.js
    removed necessary code

--- 2_Player_Asteroids_2024_04_11_23_22_26/laser2.js
+++ 2_Player_Asteroids_2024_04_11_23_22_26/laser2.js
@@ -14,15 +14,6 @@ function Laser2(spos, angle) {
     point(this.pos.x, this.pos.y);
     pop();
   }
-
-  //this.hits = function(asteroid) {
-   // var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
-   // if (d < asteroid.r) {
-   //   return true;
-   // } else {
-   //   return false;
-   // }
- // }
   
    this.hits = function(asteroid2) {
     var d = dist(this.pos.x, this.pos.y, asteroid2.pos.x, asteroid2.pos.y);
